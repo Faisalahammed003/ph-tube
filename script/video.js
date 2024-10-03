8909aabab3e5be9d542129104b251e13bc1d27de
/Faisalahammed003 <faisalahammed5035@gmail.com>
2024-10-04 02:50:34 +0600

    display completed

--- script/video.js
+++ script/video.js
@@ -1,3 +1,13 @@
+// !Date Funcatuion
+
+function gettingTime(time) {
+  const hour = parseInt(time / 3600);
+  let remaningSecond = time % 3600;
+  const munite = parseInt(time / remaningSecond);
+  remaningSecond = remaningSecond % 60;
+  return `${hour} Hour ${munite} Minute ${remaningSecond} Second ago`;
+}
+
 //!create loadCatagories
 
 //button add--->
@@ -45,17 +55,35 @@ const displayVideo = (videos) => {
     // creat Video-------->
     const card = document.createElement("div");
     card.classList = "card card-compact";
-    card.innerHTML = `  <figure class="h-[200px]">
+    card.innerHTML = `<figure class="h-[200px] relative">
     <img class="h-full w-full object-cover"
       src="${video.thumbnail}"
       alt="Shoes" />
+      ${
+        video.others.posted_date?.length == 0
+          ? ""
+          : `<span class="absolute right-2 bottom-2 bg-black text-white text-xs rounded p-1">${gettingTime(
+              video.others.posted_date
+            )}</span>`
+      }
+        
   </figure>
-  <div class="px-0 py-2">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
+  <div class="px-0 py-2 flex gap-2">
+      <div><img class="w-10 h-10 rounded-full object-cover" src=${
+        video.authors[0].profile_picture
+      }></div>
+        <div>
+            <h2 class="font-bold">${video.title}</h2>
+            <div class="flex items-center gap-2">
+            <p>${video.authors[0].profile_name}</p>
+            ${
+              video.authors[0].verified === true
+                ? `<img class="w-5" src="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000" alt="">`
+                : ""
+            }
+            <div/>
+            <p></p>
+        </div>
   </div>`;
     videoContainer.append(card);
   });
@@ -65,3 +93,24 @@ const displayVideo = (videos) => {
 
 load();
 videoLoad();
+
+/* {
+  "status": true,
+  "message": "successfully fetched all the videos",
+  "videos": [
+    {
+      "category_id": "1001",
+      "video_id": "aaaa",
+      "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+      "title": "Shape of You",
+      "authors": [
+        {
+          "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+          "profile_name": "Olivia Mitchell",
+          "verified": ""
+        }
+      ],
+      "others": {
+        "views": "100K",
+        "posted_date": "16278"
+      },*/
